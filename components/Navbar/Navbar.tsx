f8e03b7c9769a3b29bc1e622cbeb5138cc122643
sasatank <sasa.tankytank@gmail.com>
2023-08-27 00:09:42 +0900

    タイトル変更5

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,22 +3,25 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className='container mx-auto lg:px-2 px-5 lg:w-2/5'>
-      <div className='container font-medium">SHO</Link>
+    
+    <nav className="container mx-auto lg:px-2 px-5 lg:w-2/5">
+      <div className='container flex items-center justify-between mx-auto'>
+      <Link href="/" className="text-2xl font-medium">SHO</Link>
         <div>
         <ul className="flex items-center text-sm py-4">
             <li>
               <Link href="/" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300">Home</Link>
             </li>
             <li>
-            <a href='https://twitter.com/Sho_progra' className='hover:text-sky-900 transition-all duration-300'>X(Twitter)</a>
+              <Link href="https://twitter.com/Sho_progra" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300">X(Twitter)</Link>
             </li>
          
-          </ul>
-        </div>
-      </div>
+          </ul >
+        </div >
+      </div >
     </nav>
-  )
+    
+  );
 }
 
-export default Navbar;
+export default Navbar
