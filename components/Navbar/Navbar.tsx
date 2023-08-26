97ecb52f0754e80f07cda50926887709053a0f5d
sasatank <sasa.tankytank@gmail.com>
2023-08-26 23:48:43 +0900

    タイトル変更2

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className='container mx-auto lg:px-2 px-5 lg:w-2/5'>
       <div className='container font-medium">SHO</Link>
         <div>
-          <ul className='flex items-center text-sm py-4'>
+        <ul className="flex items-center text-sm py-4">
             <li>
               <Link href="/" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300">Home</Link>
             </li>
@@ -21,4 +21,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
